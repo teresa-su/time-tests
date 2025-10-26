fb064feadf6ab6c20fac998ecf2e7336cf5ab585
Teresa Su <2002tsu@gmail.com>
2025-10-26 11:58:19 +0000

    Add .gitignore file

--- times.py
+++ times.py
@@ -1,7 +1,10 @@
 import datetime
-
+import pytest
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
+    """
+    Calculate duration between two times and create a list of interval tuples
+    """
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
@@ -10,8 +13,10 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
                  for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
-
 def compute_overlap_time(range1, range2):
+    """
+    Finds all overlapping periods between 2 set of time ranges
+    """
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
@@ -24,3 +29,17 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
+
+def test_time_range_and_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 0)
+    result = compute_overlap_time(large, short)
+    
+    expected = [
+        ("2010-01-12 10:30:00", "2010-01-12 10:45:00"),
+        ("2010-01-12 10:30:00", "2010-01-12 10:45:00"),
+        ("2010-01-12 11:00:00", "2010-01-12 11:15:00"),
+        ("2010-01-12 11:00:00", "2010-01-12 11:15:00")
+    ]
+    
+    assert result == expected
