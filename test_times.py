cefa2b5230a6548ea0cbe9cd2b41c192b3b19621
Teresa Su <2002tsu@gmail.com>
2025-11-07 22:39:56 +0000

    Add additional tests to ensure valid time ranges, and handle intervals with no overlap correctly

--- test_times.py
+++ test_times.py
@@ -1,24 +1,41 @@
 from datetime import datetime
-import times
+from times import time_range, compute_overlap_time
 from pytest import raises
+
+def test_generic_case():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected
 
 def test_given_input():
     """
     A unit test on the times.py file
     """
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
-    short = times.time_range("2010-01-12 11:00:00", "2010-01-12 14:00:00", 3, 0)
-    result = times.compute_overlap_time(large, short)
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
+    short = time_range("2010-01-12 11:00:00", "2010-01-12 14:00:00", 3, 0)
+    result = compute_overlap_time(large, short)
     expected = [('2010-01-12 11:00:00', '2010-01-12 12:00:00')]
     assert result == expected
 
-# def test_no_overlap_input():
-#     """
-#     A unit test for non-overlapping periods
-#     """
-#     range1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
-#     range2 = times.time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00")
+def test_no_overlap_input():
+    """
+    A unit test for non-overlapping periods
+    """
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
+    range2 = time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00")
 
-#     # Raise error when trying to compute overlap period for non-overlapping inputs
-#     with raises(ValueError):
-#         times.compute_overlap_time(range1, range2)
+    # Raise error when trying to compute overlap period for non-overlapping inputs
+    result = compute_overlap_time(range1, range2)
+    expected = []
+    assert result == expected
+
+def test_common_start_end_time():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00", 2, 60)
+    expected = []
+    assert compute_overlap_time(large, short) == expected
+
+def test_valid_time_range():
+    with raises(ValueError):
+        range = time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
--- times.py
+++ times.py
@@ -7,10 +7,17 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
     """
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    # Add test to check whether given time ranges actually overlap
+
+    if end_time_s <= start_time_s:
+        raise ValueError(f"Start time must be before end time")
+
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
+                start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
+                for i in range(number_of_intervals)]
+
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 def compute_overlap_time(range1, range2):
@@ -23,10 +30,8 @@ def compute_overlap_time(range1, range2):
             low = max(start1, start2)
             high = min(end1, end2)
             if low < high:
-            # Add test to check whether given time ranges actually overlap
-            # if low >= high:
-            #     raise ValueError(f"Non-overlapping time ranges: {(start1, end1)} and {(start2, end2)}")
                 overlap_time.append((low, high))
+            
     return overlap_time
 
 large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
