7f23dee98c4fbb21b72812fdec7c6c95009257b1
Teresa Su <2002tsu@gmail.com>
2025-11-08 20:37:38 +0000

    Parametrise tests to avoid repetition

--- test_times.py
+++ test_times.py
@@ -1,41 +1,40 @@
 from datetime import datetime
 from times import time_range, compute_overlap_time
+import pytest
 from pytest import raises
 
-def test_generic_case():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
-    assert compute_overlap_time(large, short) == expected
+@pytest.mark.parametrize("range1, range2, expected", 
+[
+# General overlap case
+(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+(time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)),
+[("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]),   
 
-def test_given_input():
+# Both ranges with multiple periods
+(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+ time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+ [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]),
+
+# Common start end time
+(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+ time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00", 2, 60),
+ []),
+
+# No-overlap
+(time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00"),
+ time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00"),
+ [])])
+
+def test_compute_overlap_time(range1, range2, expected):
     """
-    A unit test on the times.py file
+    Parametrised unit for for compute_overlap_time()
     """
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
-    short = time_range("2010-01-12 11:00:00", "2010-01-12 14:00:00", 3, 0)
-    result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 11:00:00', '2010-01-12 12:00:00')]
+    result = compute_overlap_time(range1,range2)
     assert result == expected
-
-def test_no_overlap_input():
-    """
-    A unit test for non-overlapping periods
-    """
-    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
-    range2 = time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00")
-
-    # Raise error when trying to compute overlap period for non-overlapping inputs
-    result = compute_overlap_time(range1, range2)
-    expected = []
-    assert result == expected
-
-def test_common_start_end_time():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00", 2, 60)
-    expected = []
-    assert compute_overlap_time(large, short) == expected
 
 def test_valid_time_range():
+    """
+    Negative test: end time must be after start time. Otherwise raise error
+    """
     with raises(ValueError):
         range = time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
